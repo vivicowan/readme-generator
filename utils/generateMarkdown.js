cbc6848ca22b43317fd7c3250440542003768b76
Vivi Cowan <vivicare@gmail.com>
2021-07-09 16:19:19 -0700

    created outline for generating markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -56,9 +56,18 @@ ${renderDescriptionSection(data.description1, data.description2, data.descriptio
 
 ## Installation
 
+${renderInstallationSection(data.installation)}
+
+## Usage
+
+## Contribution
+
 ## License
 
 ${renderLicenseSection(data.license)}
+
+## Tests
+
 `;
 }
 
@@ -66,7 +75,7 @@ ${renderLicenseSection(data.license)}
 
 // 
 // 
-// ## Usage
+// 
 // Provide instructions and examples for use. Include screenshots as needed.
 // To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
 //     ```md
@@ -77,9 +86,9 @@ ${renderLicenseSection(data.license)}
 // If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
 // If you followed tutorials, include links to those here as well.
 
-// ## How to Contribute
+// 
 // If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
-// ## Tests
+// 
 // Go the extra mile and write tests for your application. Then provide examples on how to run them here.
 
 
